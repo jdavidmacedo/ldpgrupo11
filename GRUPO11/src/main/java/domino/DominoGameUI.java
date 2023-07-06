425573ab287c96e90e3d66d0e1963a6483aedf90
jdavidmacedo <85846966+jdavidmacedo@users.noreply.github.com>
2023-07-06 23:07:38 +0100

    Imports2

--- GRUPO11/src/main/java/domino/DominoGameUI.java
+++ GRUPO11/src/main/java/domino/DominoGameUI.java
@@ -10,6 +10,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.Random;
 
