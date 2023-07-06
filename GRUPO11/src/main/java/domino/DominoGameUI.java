43ba3c87aed71b7dfd3c3dc1b74419a31aca2284
jdavidmacedo <85846966+jdavidmacedo@users.noreply.github.com>
2023-07-06 23:31:21 +0100

    Update DominoGameUI.java

--- GRUPO11/src/main/java/domino/DominoGameUI.java
+++ GRUPO11/src/main/java/domino/DominoGameUI.java
@@ -7,6 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
